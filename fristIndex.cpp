f3ca1adeab9f5139ae31ccedfe81195ca6882921
shakeeb-droids <shakeebshahid9@gmail.com>
2021-12-10 00:55:46 +0530

    Recursion

---
+++ fristIndex.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+int firstIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    //base case
+    if(size==0)
+    {
+        return -1;
+    }
+    if(input[0]==x)
+    {
+       return 0;
+    }
+    //make smaller recursive calls
+    int smallAns = firstIndex(input+1, size-1, x);
+    //our calculation based on small recursive call
+    if(smallAns>=0)
+    return smallAns + 1;
+    else
+    return -1;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    cout << firstIndex(input, n, x) << endl;
+
+}
+
+
