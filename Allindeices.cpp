0f7be3086bb5be7e545076bb53f247e56e0252b9
shakeeb-droids <shakeebshahid9@gmail.com>
2021-12-10 01:00:12 +0530

    Recursion

---
+++ Allindeices.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include "Solution.h"
+using namespace std;
+
+int lastIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    if(size==0)
+    {
+        return -1;
+    }
+    int smallAns = lastIndex(input+1, size-1, x);
+    if(smallAns!=-1)
+        return smallAns+1;
+    if(input[0] == x)
+        return 0;
+    else
+        return -1;
+
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    int *output = new int[n];
+    
+    int size = allIndexes(input, n, x, output);
+    for(int i = 0; i < size; i++) {
+        cout << output[i] << " ";
+    }
+    
+    delete [] input;
+    
+    delete [] output;
+    
+
+}
+
+
